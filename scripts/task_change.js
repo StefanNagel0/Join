4b7364ed2f824288d9655e35077354a24554a32d
IcePain <143926986+BadPain@users.noreply.github.com>
2025-01-05 19:40:55 +0100

    hotfix

--- scripts/task_change.js
+++ scripts/task_change.js
@@ -2,37 +2,11 @@ function onload() {
     loadTask();
 }
 
+
 const BASE_URL = "https://join-408-default-rtdb.europe-west1.firebasedatabase.app/";
 
-function loadTask() {
-    const tasksRef = firebase.database().ref('tasks');
-    tasksRef.on('value', (snapshot) => {
-      const tasks = snapshot.val();
-      displayTasks(tasks);
-    });
-  }
-
-
-async function createTask() {
-    const title = document.getElementById('task-title').value;
-    const description = document.getElementById('task-desc').value;
-    const assignedTo = document.getElementById('task-assigned').value;
-    const dueDate = document.getElementById('task-date').value;
-    const priority = document.getElementById('task-priority-hidden').value;
-    const category = document.getElementById('task-category').value;
-    
-    const newTaskRef = firebase.database().ref('tasks').push();
-    
-    await newTaskRef.set({
-      title,
-      description,
-      assignedTo,
-      dueDate,
-      priority,
-      category,
-      status: 'To do'
-    });
-  
-    addTaskSuccess();
-    loadTask();
-  }
+async function loadTask(path="") {
+    let response = await fetch(BASE_URL + path + ".json");
+    let responseToJson = await response.json();
+    console.log(responseToJson);
+}

9aef0e9ffefc487f376cbff8a0c1ce731661b6be
StefanNagel0 <stefannagel14@hotmail.de>
2025-01-25 21:01:50 +0100

    header JSDoc done

--- scripts/header.js
+++ scripts/header.js
@@ -1,12 +1,15 @@
+/**Initializes the header by setting up the user button.*/
 function initHeader() {
     initializeUserButton();
 }
 
+/**Initializes the user button and sets the user initials.*/
 function initializeUserButton() {
     const userInitialsButton = document.getElementById('user-initials-button');
     setUserInitials(userInitialsButton);
 }
 
+/*** Sets the user initials to the button text content.*/
 function setUserInitials(button) {
     const userName = getCurrentUserName();
     const initials = userName.toLowerCase() === "guest" 
@@ -15,6 +18,7 @@ function setUserInitials(button) {
     button.textContent = initials;
 }
 
+/**Retrieves the current user's name based on the logged-in email.*/
 function getCurrentUserName() {
     const loggedInEmail = localStorage.getItem('loggedInEmail');
     if (!loggedInEmail || loggedInEmail === 'guest@example.com') {
@@ -24,18 +28,21 @@ function getCurrentUserName() {
     return user ? user.name : "Guest";
 }
 
+/**Generates initials from a full name by taking the first letter of the first and last name.*/
 function getInitials(name) {
     if (!name) return '';
     const [firstName, lastName] = name.split(" ");
     return `${firstName?.[0] || ""}${lastName?.[0] || ""}`;
 }
 
+/**Toggles the visibility of the user popup.*/
 function toggleUserPopup(event) {
     const userPopup = document.getElementById('user-popup');
     event.stopPropagation();
     userPopup.classList.toggle('d-none');
 }
 
+/**Closes the user popup if the user clicks outside the popup or the user initials button.*/
 function closePopup(event) {
     const userPopup = document.getElementById('user-popup');
     const userInitialsButton = document.getElementById('user-initials-button');
@@ -44,4 +51,6 @@ function closePopup(event) {
         userPopup.classList.add('d-none');
     }
 }
+
+/**Initializes the header by calling the `initHeader` function when the document is loaded.*/
 document.onload = initHeader;

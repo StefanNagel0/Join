55ae4e95528c2db5329834c2850b37f929f39d9f
Taylan5567 <59730168+Taylan5567@users.noreply.github.com>
2025-01-26 19:57:57 +0100

    FIXXXXXX

--- scripts/board_template.js
+++ scripts/board_template.js
@@ -81,6 +81,7 @@ function boardAddTaskTemplate() {
     return `
 <section>
 <form id="add-task-form" onsubmit="event.preventDefault(); postTask();">
+         <form id="add-task-form" onsubmit="event.preventDefault(); postTask();">
         <section class="add_task_form_box">
             <div class="add_task_left_box addTaskLeftBoxBoard">
                 <div class="gap_8">
@@ -94,8 +95,12 @@ function boardAddTaskTemplate() {
                 <div class="gap_8">
                     <label for="task-assigned">Assigned to</label>
                     <div id="task-assigned" style="border: 1px solid #ccc; padding: 10px;"></div>
-                </div> 
-                <div class="add_task_right_box">
+                </div>
+            </div>
+            <div class="add_task_mid_box">
+                <img src="../assets/svg/add_task/add_tastk_vertical_line.svg" alt="">
+            </div>
+            <div class="add_task_right_box">
                 <div class="gap_8">
                     <label for="task-date">Due Date <span class="red_star">*</span></label>
                     <input type="date" class="task-date" id="task-date" name="dueDate" required>
@@ -141,84 +146,27 @@ function boardAddTaskTemplate() {
                         <img id="add-subtask" src="../assets/svg/add_task/add+symbol.svg" alt="">
                     </div>
                     <ul id="subtask-list"></ul>
-                </div>   
-<section class="add_task_form_box">
-        <div class="add_task_left_box addTaskLeftBoxBoard">
-            <div class="gap_8">
-                <label for="task-title">Title <span class="red_star">*</span></label>
-                <input maxlength="30" type="text" id="task-title" name="title" placeholder="Enter a title" required>
-            </div>
-             <div class="add_task_mid_box">
-                <img src="../assets/svg/add_task/add_tastk_vertical_line.svg" alt="">
-            <div class="gap_8">
-                <label for="task-desc">Description</label>
-                <textarea maxlength="150" id="task-desc" name="description" placeholder="Enter a Description"></textarea>
-            </div>
-            <div class="gap_8">
-                <label for="task-assigned">Assigned to</label>
-                <div id="task-assigned" style="border: 1px solid #ccc; padding: 10px;"></div>
-            </div>
-        </div>
-        <div class="add_task_mid_box">
-            <img src="../assets/svg/add_task/add_tastk_vertical_line.svg" alt="">
-        </div>
-        <div class="add_task_right_box">
-            <div class="gap_8">
-                <label for="task-date">Due Date <span class="red_star">*</span></label>
-                <input type="date" class="task-date" id="task-date" name="dueDate" required>
-            </div>
-            <div class="gap_8">
-                <p class="prio_text">Prio</p>
-                <div id="task-priority">
-                                    <button type="button" class="prio-btn urgent" data-prio="urgent">Urgent <img
-                                            src="../assets/svg/add_task/prio_urgent.svg" alt=""></button>
-                                    <button type="button" class="prio-btn medium" data-prio="medium">Medium <img
-                                            src="../assets/svg/add_task/prio_medium.svg" alt=""></button>
-                                    <button type="button" class="prio-btn low" data-prio="low">Low <img
-                                            src="../assets/svg/add_task/prio_low.svg" alt=""></button>
-                                </div>
-            </div>
-                <div class="dropdown-container">
-                                <div class="gap_8">
-                                    <label for="task-category">Category <span class="red_star">*</span></label>
-                                    <div class="dropdown-container">
-                                        <div class="custom-dropdown" id="dropdown-toggle-category"
-                                            onclick="toggleDropdown(event, this, document.getElementById('dropdown-options-category'))">
-                                            <span>Select task category</span>
-                                            <div class="dropdown-arrow"></div>
-                                        </div>
-                                        <ul required class="dropdown-options hidden" id="dropdown-options-category">
-                                            <li class="dropdown-option" data-category="User Story"
-                                                onclick="selectDropdownOption(event, document.getElementById('dropdown-toggle-category'), this)">
-                                                User Story</li>
-                                            <li class="dropdown-option" data-category="Technical Task"
-                                                onclick="selectDropdownOption(event, document.getElementById('dropdown-toggle-category'), this)">
-                                                Technical Task</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-            <div class="gap_8">
-                <label for="task-subtasks">Subtasks</label>
-                <div id="subtask-container">
-                    <input maxlength="20" type="text" id="new-subtask" placeholder="Add new subtask">
-                    <img id="clear-subtask" class="d-none" src="../assets/svg/add_task/closeXSymbol.svg" alt="">
-                    <img id="add-subtask" src="../assets/svg/add_task/add+symbol.svg" alt="">
                 </div>
-                <ul id="subtask-list"></ul>
+            </div>
+        </section>
+        <div class="add_task_required_info_box_overlay">
+            <div>
+                <span class="add_task_required_info">
+                <span class="red_star">*</span>
+                    This field is required
+                </span>
+            </div>
+            <div class="addTaskBottomBox">
+                <button type="submit" id="addTaskButton" class="addTaskButton">
+                    <p class="addTaskButtonText">Add Task</p>
+                    <img class="addTaskButtonImgBottomBox" src="../assets/svg/add_task/check.svg" alt="">
+                </button>
+                <button type="button" id="cancelTaskButton" class="cancelAddTaskButton" onclick="closeBoardAddTask()">
+                    <p class="cancelTaskButtonText">Cancel X</p>
+                </button>
             </div>
         </div>
-    </section>
-    <p class="requiredFields">* Required fields</p>
-    <div class="addTaskBottomBox">
-        <Button id="addTaskButton" class="addTaskButton" onclick="postTask()">
-            <p class="addTaskButtonText">Add Task</p>
-            <img class="addTaskButtonImgBottomBox" src="../assets/svg/add_task/check.svg" alt="">
-        </Button>
-        <Button id="cancelTaskButton" class="cancelAddTaskButton" onclick="closeBoardAddTask()">
-            <p class="cancelTaskButtonText">Cancel X</p>
-        </Button>
-    </div>
+    </form>
 </section>
 `
 }
